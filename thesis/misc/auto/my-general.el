924c47d9ebb3829182b54f02350a2eb1e60e7e02
Alex Kreimer <alex.kreimer@gmail.com>
2017-11-19 16:05:01 +0200

    update

--- thesis/misc/auto/my-general.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "my-general"
- (lambda ()
-   (LaTeX-add-environments
-    '("proofsketchof" LaTeX-env-args ["argument"] 0)
-    '("proofof" LaTeX-env-args ["argument"] 0)
-    '("proof" LaTeX-env-args ["argument"] 0)))
- :plain-tex)
-
